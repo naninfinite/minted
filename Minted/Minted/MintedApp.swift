b5b5fb2c0fcae97173ef9ed0b6efd14704021edc
naninfinite <215418892+naninfinite@users.noreply.github.com>
2025-09-16 18:26:14 +0100

    init(app): add Minted Xcode project skeleton with unit tests

---
+++ Minted/Minted/MintedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MintedApp.swift
+//  Minted
+//
+//  Created by NaNinfinite on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MintedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
